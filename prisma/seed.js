86f606cfb18ec50fdebada05df107640a906c5ab
Tejaswa Dayal <tejaswadayalsri@gmail.com>
2024-06-15 22:22:19 +0530

    Added custom script (migrate) for prisma migration and added seed.js file to add default admin user.

---
+++ prisma/seed.js
@@ -0,0 +1,34 @@
+const bcrypt = require('bcrypt');
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+async function main() {
+  const chalk = (await import('chalk')).default; // For CLI colors
+  const user = await prisma.user.findUnique({
+    where: {
+      email: 'admin@smrealty.com', 
+    },
+  })
+
+  if (!user) {
+    const hashedPassword = await bcrypt.hash('Smrealtyadmin', 10); // Hash the password
+    await prisma.user.create({
+      data: {
+        email: 'admin@smrealty.com', 
+        password: hashedPassword,
+      },
+    })
+    console.log(chalk.green('Default user created'))
+  } else {
+    console.log(chalk.red('Default user already exists'))
+  }
+}
+
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
